80f036b433895024d100598bf6ea3146dd3fddd2
Nicolas Guadagno <nicolasguadagno@gmail.com>
2024-06-16 18:12:02 -0300

    feat(01): Importing remove modules

--- 01/hello-world/go.mod
+++ 01/hello-world/go.mod
@@ -1,3 +1,5 @@
 module hello
 
 go 1.22.4
+
+require github.com/google/go-cmp v0.6.0
--- 01/hello-world/hello.go
+++ 01/hello-world/hello.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/go-cmp/cmp"
 	"hello/morestrings"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
